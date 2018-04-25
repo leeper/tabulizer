3932b1a86dd9fef16ce08bc2262f5b5a3b15d0a2
Tom Paskhalis <tpaskhalis@gmail.com>
2018-04-25 14:29:25 -0400

    Redo non-latin tests

--- tests/testthat/test_non-latin.R
+++ tests/testthat/test_non-latin.R
@@ -1,19 +1,60 @@
-context("Non-latin character tests")
+context("Non-latin characters tests")
 
-test_that("Read Spanish language PDF", {
-    f1 <- "https://github.com/tabulapdf/tabula-java/raw/98957221950af4b90620b51a29e0bbe502eea9ad/src/test/resources/technology/tabula/argentina_diputados_voting_record.pdf"
-    expect_true(is.matrix(extract_tables(f1, pages = 1, area = list(c(269.875, 12.75, 790.5, 561)))[[1]]))
-    t1a <- extract_tables(f1, pages = 1, area = list(c(269.875, 12.75, 790.5, 561)), output = "data.frame", encoding = "latin1")
-    #expect_true(names(t1a[[1]])[2] == "Frente.CÃ.vico.por.Santiago", label = "latin1 encoding worked")
-    t1b <- extract_tables(f1, pages = 1, area = list(c(269.875, 12.75, 790.5, 561)), output = "data.frame", encoding = "UTF-8")
-    #expect_true(names(t1b[[1]])[2] == "Frente.Cívico.por.Santiago", label = "UTF-8 encoding worked")
-    
+sf <- system.file("examples", "text_non_latin.pdf", package = "tabulizer")
+
+test_that("Extract text in Spanish", {
+  txtes <- extract_text(sf, area = list(c(195, 125, 218, 401)))
+  txtes <- gsub("[^[:print:]]", "", txtes)
+  # Force NFD form
+  sentes <- "Cada vez que me trabo, F\u0065\u0301lix paga un whisky a\u006e\u0303ejo."
+  expect_identical(txtes, sentes)
 })
 
-test_that("Read French language PDF w/correct encoding", {
-    f2 <- "http://www.europarl.europa.eu/oeil/popups/printfichetechnical.pdf?id=673511&lang=fr"
-    t2a <- extract_text(f2, page = 1, encoding = "latin1")
-    t2b <- extract_text(f2, page = 1, encoding = "UTF-8")
-    #expect_true(nchar(strsplit(t2a, "\n")[[1]][1]) == 50, label = "latin1 encoding worked")
-    #expect_true(nchar(strsplit(t2b, "\n")[[1]][1]) == 47, label = "UTF-8 encoding worked")
+test_that("Extract text as table in Spanish", {
+  tablees <- extract_tables(sf, area = list(c(195, 125, 218, 401)), guess = FALSE)
+  tablees <- unlist(gsub("[^[:print:]]", "", tablees))
+  sentes <- "Cada vez que me trabo, Félix paga un whisky añejo."
+  expect_identical(tablees, sentes)
+})
+
+test_that("Extract text in Russian", {
+  txtru <- extract_text(sf, area = list(c(268, 123, 288, 470)))
+  txtru <- gsub("[^[:print:]]", "", txtru)
+  sentru <- "Съешь же ещё этих мягких французских булок да выпей чаю."
+  expect_identical(txtru, sentru)
+})
+
+test_that("Extract text as table in Russian", {
+  tableru <- extract_tables(sf, area = list(c(268, 123, 288, 470)), guess = FALSE)
+  tableru <- unlist(gsub("[^[:print:]]", "", tableru))
+  sentru <- "Съешь же ещё этих мягких французских булок да выпей чаю."
+  expect_identical(tableru, sentru)
+})
+
+test_that("Extract text in Greek", {
+  txtel <- extract_text(sf, area = list(c(340, 127, 359, 349)))
+  txtel <- gsub("[^[:print:]]", "", txtel)
+  sentel <- "Ξεσκεπάζω την ψυχοφθόρα σας βδελυγ\u00b5ία."
+  expect_identical(txtel, sentel)
+})
+
+test_that("Extract text as table in Greek", {
+  tableel <- extract_tables(sf, area = list(c(340, 127, 359, 349)), guess = FALSE)
+  tableel <- unlist(gsub("[^[:print:]]", "", tableel))
+  sentel <- "Ξεσκεπάζω την ψυχοφθόρα σας βδελυγμία."
+  expect_identical(tableel, sentel)
+})
+
+test_that("Extract text in Japanese", {
+  txtjp <- extract_text(sf, area = list(c(410, 107, 447, 505)))
+  txtjp <- gsub("[^[:print:]]", "", txtjp)
+  sentjp <- "いろはにほへと ちりぬるを わかよたれそ つねならむ うゐのおくやま けふこえて あさきゆめみし ゑひもせす."
+  expect_identical(txtjp, sentjp)
+})
+
+test_that("Extract text as table in Japanese", {
+  tablejp <- extract_tables(sf, area = list(c(410, 107, 447, 505)), guess = FALSE)
+  tablejp <- paste(unlist(tablejp), collapse = "")
+  sentjp <- "いろはにほへと ちりぬるを わかよたれそ つねならむ うゐのおくやま けふこえて あさきゆめみし ゑひもせす."
+  expect_identical(tablejp, sentjp)
 })
